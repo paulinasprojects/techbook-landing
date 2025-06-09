836fc1c33230105f2ba7fe5f9cfee9fd9ba4cf9e
paulinasprojects <virtualpaula93@gmail.com>
2025-06-09 09:53:56 +0300

    initialized new app with vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { MainLayout } from "./components/layouts/main-layout"
+import { Homepage } from "./components/pages/home-page"
+
+function App() {
+  return (
+    <MainLayout>
+      <Homepage/>
+    </MainLayout>
+  )
+}
+
+export default App
